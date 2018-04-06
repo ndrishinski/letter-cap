8e8920ff3e300373181daab932fffcf9fd2b6c27
Nick Drishinski <ndrishinski@yahoo.com>
2018-04-06 16:36:29 -0600

    added devise, created nav and styled, fixed buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'pages#home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  
+
   resources :letters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
